79799720d4c1fb3ee618b673657961a29e1f241e
Nikunj Jain <nikunjjain@Nikunjs-MacBook-Air.local>
2025-12-02 09:10:56 +0530

    first commit

---
+++ src/pages/Dashboard/DashboardPage.js
@@ -0,0 +1,49 @@
+import { toast } from "react-toastify";
+import { useState } from "react";
+import { useEffect } from "react";
+import { DashboardCard } from "./components/DashboardCard";
+import { DashboardEmpty } from "./components/DashboardEmpty";
+import { getUserOrders } from "../../services";
+import { useTitle } from "../../hooks/useTitle";
+
+export const DashboardPage = () => {
+  useTitle("Order Summary");
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    async function fetchOrders() {
+      try {
+        const data = await getUserOrders();
+        setOrders(data);
+      } catch (error) {
+        toast.error(error.message, {
+          position: "bottom-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          theme: "light",
+        });
+      }
+    }
+    fetchOrders();
+  }, []);
+
+  return (
+    <main>
+      <section>
+        <p className="text-2xl text-center font-semibold dark:text-slate-100 my-10 underline underline-offset-8">
+          My Dashboard
+        </p>
+      </section>
+
+      <section>
+        {orders.length &&
+          orders.map((order) => <DashboardCard key={order.id} order={order} />)}
+      </section>
+
+      <section>{!orders.length && <DashboardEmpty />}</section>
+    </main>
+  );
+};
