4523ee0a0b27859b9e0c3e1f6bf5fe7dbdd75d79
Nikunj Jain <nikunjjain@Nikunjs-MacBook-Air.local>
2025-12-02 23:37:59 +0530

    Added login as guest

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,6 +9,16 @@ export const Login = () => {
   const navigate = useNavigate();
   const email = useRef();
   const password = useRef();
+  async function handleLoginAsGuest() {
+    email.current.value = "nikunj.jain@gmail.com";
+    password.current.value = "password";
+    const authDetail = {
+      email: email.current.value,
+      password: password.current.value,
+    };
+    const data = await login(authDetail);
+    data.accessToken ? navigate("/products") : toast.error(data);
+  }
 
   async function handleLogin(event) {
     event.preventDefault();
@@ -74,7 +84,12 @@ export const Login = () => {
           Log In
         </button>
       </form>
-      {/* <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> */}
+      <button
+        className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        onClick={handleLoginAsGuest}
+      >
+        Login As Guest
+      </button>
     </main>
   );
 };
